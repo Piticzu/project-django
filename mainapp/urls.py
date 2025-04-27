3a753930ffae327c8bb0204483b1ae8671a43e98
Piticzu <151443048+Piticzu@users.noreply.github.com>
2025-04-27 15:51:08 +0300

    Refactor chat system: move HTML to templates, implement session-based chat history, improve scroll behavior with HTMX, fix security escaping

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import index_view, send_message_view, scroll_anchor_view
+from .views import index_view, send_message_view, bot_response_view
 
 urlpatterns = [
     path('', index_view, name='index'),
     path('send/', send_message_view, name='send'),
-    path('scroll-anchor/', scroll_anchor_view, name='scroll-anchor'),
+    path('bot-response/', bot_response_view, name='bot-response'),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -2,35 +2,51 @@
 from .utils import call_gpt
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template.loader import render_to_string
 from django.views.decorators.http import require_POST, require_GET
 from django.views.decorators.csrf import csrf_exempt
 
-def scroll_anchor_view(request):
-    return HttpResponse()
-
 @require_GET
 def index_view(request):
-    return render(request, "index.html")
+    chat_history = request.session.get('chat_history', [])
+    return render(request, "index.html", {'chat_history': chat_history})
 
 @csrf_exempt
 @require_POST
 def send_message_view(request):
     user_message = request.POST.get('message', '')
+
+    if 'chat_history' not in request.session:
+        request.session['chat_history'] = []
+
+    request.session['chat_history'].append({
+        'role': 'user',
+        'content' : user_message
+    })
+    request.session.modified  = True
+
+    html = render_to_string('user_message_block.html', {
+        'user_message': user_message,
+        'hash' : hash(user_message)
+    })
+
+    return HttpResponse(html)
+@require_GET
+def bot_response_view(request):
+    user_message = request.GET.get('message', '')
     bot_response = call_gpt(user_message)
 
-    return HttpResponse(f"""
-        <div class="flex justify-end">
-            <div class="bg-neutral-700 text-white px-4 py-2 rounded-l-full rounded-tr-full max-w-xs">
-                {user_message}
-            </div>
-        </div>
-        <div class="text-left text-white">{bot_response}</div>
+    if 'chat_history' not in request.session:
+        request.session['chat_history'] = []
 
-        <script>
-            const form = document.querySelector('form');
-            if (form) {{
-                form.reset();
-                form.querySelector('input[name=message]').focus();
-            }}
-        </script>
-    """)
+    request.session['chat_history'].append({
+        'role': 'assistant',
+        'content' : bot_response
+    })
+    request.session.modified  = True
+
+    html = render_to_string('bot_response_block.html', {
+        'bot_response': bot_response
+    })
+
+    return HttpResponse(html)
